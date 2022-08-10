c880343fdfdfa982fa6103a13b3b85243e3b1142
omoslakova <o.moslakova@yandex.by>
2022-08-10 12:50:50 +0300

    local state

--- src/Components/Profile/ProfileContainer.jsx
+++ src/Components/Profile/ProfileContainer.jsx
@@ -32,7 +32,7 @@ let mapStateToProps = (state) => ({
 export default compose (
     connect(mapStateToProps, {getUserProfile}),
     withRouter,
-    withAuthRedirect
+    //withAuthRedirect
 )(ProfileContainer);
 
 
--- src/Components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
+import ProfileStatus from "./ProfileStatus"
 
 
-const Profileinfo = (props) => {
+const ProfileInfo = (props) => {
     if (!props.profile) {
         return <Preloader />
     }
 
     return (
         <div>
-            <div>
+            {/*<div>
                 <img
                     src='https://blog.onlime.ru/wp-content/uploads/2019/12/shutterstock_529670182-945x630.jpg'></img>
-            </div>
+            </div>*/}
             <div className={s.descriptionBlock}>
                <img src={props.profile.photos.large} />
-               AVA+D
+               <ProfileStatus status={"Hello my friends"}/>
             </div>
-            <div>
-            </div>
+            
         </div>)
 }
 
-export default Profileinfo
+export default ProfileInfo
