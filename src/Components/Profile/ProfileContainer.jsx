2007cbadaa4acb905eb5291d45af99137ad3b281
omoslakova <o.moslakova@yandex.by>
2022-08-10 17:20:46 +0300

    http put

--- src/Components/Profile/ProfileContainer.jsx
+++ src/Components/Profile/ProfileContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Profile from "./Profile";
-import {getUserProfile, setUserProfile} from "../../Redux/profile-reducer";
+import {getUserProfile, getStatus, updateStatus} from "../../Redux/profile-reducer";
 import {connect} from "react-redux";
 import {Redirect, withRouter} from "react-router-dom";
 import {usersAPI} from "../../API/api";
@@ -15,24 +15,25 @@ class ProfileContainer extends React.Component {
             userId = 2;
         }
             this.props.getUserProfile(userId);
+        this.props.getStatus(userId);
         }
 
 
     render() {
         return (
-            <Profile {...this.props} profile={this.props.profile}/>
+            <Profile {...this.props} profile={this.props.profile} status={this.props.status} updateStatus={this.props.updateStatus}/>
         )
     }
 }
 
 let mapStateToProps = (state) => ({
     profile: state.profilePage.profile,
+    status: state.profilePage.status
 });
 
 export default compose (
-    connect(mapStateToProps, {getUserProfile}),
-    withRouter,
-    //withAuthRedirect
+    connect(mapStateToProps, {getUserProfile, getStatus, updateStatus}),
+    withRouter
 )(ProfileContainer);
 
 
--- src/Redux/profile-reducer.js
+++ src/Redux/profile-reducer.js
@@ -1,8 +1,10 @@
-import {usersAPI} from "../API/api";
+import {profileAPI, usersAPI} from "../API/api";
 
 const ADD_POST = 'ADD_POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 const SET_USER_PROFILE = 'SET-USER-PROFILE';
+const SET_STATUS = 'SET_STATUS';
+
 
 let initialState = {
     posts: [
@@ -10,7 +12,8 @@ let initialState = {
         {id: 2, message: 'I am just big-boned', likesCount: 23}
     ],
     newPostText: 'frontend rocks',
-    profile: null
+    profile: null,
+    status: ""
 };
 
 const profileReducer = (state = initialState, action) => {
@@ -34,6 +37,12 @@ const profileReducer = (state = initialState, action) => {
                 newPostText: action.newText
             }
         }
+        case SET_STATUS: {
+            return {
+                ...state,
+                status: action.status
+            }
+        }
         case SET_USER_PROFILE: {
             return {...state, profile: action.profile}
         }
@@ -44,12 +53,30 @@ const profileReducer = (state = initialState, action) => {
 
 export const addPostActionCreator = () => ({type: ADD_POST})
 export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile})
-export const updateNewPostTextActionCreator = (text) =>
-    ({type: UPDATE_NEW_POST_TEXT, newText: text})
+export const setStatus = (status) => ({type: SET_STATUS, status})
+
 export const getUserProfile = (userId) => (dispatch) => {
     usersAPI.getProfile(userId).then(response => {
         dispatch(setUserProfile(response.data));
     });
 }
+export const getStatus = (userId) => (dispatch) => {
+    profileAPI.getStatus(userId)
+        .then(response => {
+            dispatch(setStatus(response.data));
+        });
+}
+
+export const updateStatus = (status) => (dispatch) => {
+    profileAPI.updateStatus(status)
+        .then(response => {
+            if (response.data.resultCode === 0) {
+                dispatch(setStatus(status));
+            }
+        });
+}
+
+export const updateNewPostTextActionCreator = (text) =>
+    ({type: UPDATE_NEW_POST_TEXT, newText: text})
 
 export default profileReducer;
